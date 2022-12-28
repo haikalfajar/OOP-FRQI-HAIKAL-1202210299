aa76b5f8443cccae553b699bb7ca740f3a987d64
haikalfajar <haikalfajar@student.telkomuniversity.ac.id>
2022-12-28 15:34:00 +0700

    MODUL 3

---
+++ MODUL3_HAIKAL/src/Main.java
@@ -0,0 +1,28 @@
+import java.lang.invoke.CallSite;
+import java.text.BreakIterator;
+
+public class Main {
+    do{
+        try{
+            switch(){
+                  case 1:
+                break;
+                  case 2:
+                break;
+                  case 3:
+                break;
+                  case 0:
+
+                default:
+            }
+            continue;
+        }
+        repeat = 
+}catch(){
+}catch(){
+}catch(){
+
+
+}
+}while();
+scannerName.close();
